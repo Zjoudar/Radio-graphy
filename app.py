71673369b223174681210cca1be58d70b482a408
Zjoudar <114192043+Zjoudar@users.noreply.github.com>
2025-01-27 13:34:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import time
 import cv2
 import uuid
 import flask
+from flask import Flask, request,render_template, redirect,session
+from flask_sqlalchemy import SQLAlchemy
+import bcrypt
 import urllib
 from PIL import Image
 from flask import Flask, render_template, request, send_file
@@ -29,10 +32,84 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET'])
-def hello_word():
-    return render_template('IndexApp.html')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+app.secret_key = 'secret_key'
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+    def __init__(self,email,password,name):
+        self.name = name
+        self.email = email
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    
+    def check_password(self,password):
+        return bcrypt.checkpw(password.encode('utf-8'),self.password.encode('utf-8'))
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def index():
+    return render_template('Home.html')
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        # handle request
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        new_user = User(name=name,email=email,password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/welcome')
+
+
+
+    return render_template('register.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+        
+        if user and user.check_password(password):
+            session['email'] = user.email
+            return redirect('/welcome')
+        else:
+            return render_template('login.html',error='Invalid user')
+
+    return render_template('login.html')
+
+@app.route('/welcome', methods=['GET'])
+def hello_world():
+    return render_template('welcome.html')
+
+@app.route('/working', methods=['GET'])
+def working():
+    return render_template('working.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('About.html')
+
+@app.route('/contact', methods=['GET'])
+def contact():
+    return render_template('contact.html')
+
+@app.route('/pricing', methods=['GET'])
+def pricing():
+    return render_template('pricing.html')
 @app.route('/', methods=['POST'])
 def predict():
     if 'imagefile' not in request.files:
